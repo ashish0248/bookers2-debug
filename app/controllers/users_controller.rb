275943cc4f53f5a0692e2d099d0eb1e0195ed1a9
vagrant <vagrant@localhost.localdomain>
2020-05-02 22:53:56 +0000

    [finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,30 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@books = @user.books
-  	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+    @book = Book.new
+    #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
   end
 
   def index
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
   end
+  
   def edit
-  	@user = User.find(params[:id])
+   @user = User.find(params[:id])
+      if @user == current_user
+
+      else
+        redirect_to user_path(current_user.id)
+      end
   end
 
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
-  		redirect_to users_path(@user), notice: "successfully updated user!"
+  		redirect_to user_path(@user.id), notice: "successfully updated user!"
   	else
-  		render "show"
+  		render "edit"
   	end
   end
 
